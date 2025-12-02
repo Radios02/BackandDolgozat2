6989e2183377062e54a3417dc336f3945042a43d
Eperjesi Vencel <b1epeven@vasvari.org>
2025-12-02 12:32:21 +0100

    ameddig jutottunk

--- data/product.js
+++ data/product.js
@@ -4,8 +4,8 @@ db.prepare(
     "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMBER, amount NUMBER)"
 ).run();
 
-export const getAllProducts = () => db.prepare(SELECT * FROM products).all(); 
+export const getAllProducts = () => db.prepare("SELECT * FROM products").all(); 
 
-export const saveProduct = (name, price, amount) => db.prepare("INSERT INTO product (name, price, amount) VALUES(?,?,?)")
+export const saveProduct = (name, price, amount) => db.prepare("INSERT INTO product (name, price, amount) VALUES(?,?,?)").run(name, price, amount); 
 
-export const updateProduct = () => db.
+export const updateProduct = (id, name, price, amount) => db.prepare("UPDATE product SET name = ?, price = ?, amount = ? WHERE id=?").run(name, price, amount, id)
