dda9b1864dc2c126d2fa4065bf9110510955e30f
Eperjesi Vencel <b1epeven@vasvari.org>
2025-12-02 12:34:12 +0100

    szépítés

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, () =>{
-    console.log("Server is running on PORT:3311!")
-})
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:3311!");
+});
--- data/product.js
+++ data/product.js
@@ -1,11 +1,17 @@
 import db from "./data.js";
 
 db.prepare(
-    "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMBER, amount NUMBER)"
+  "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMBER, amount NUMBER)"
 ).run();
 
-export const getAllProducts = () => db.prepare("SELECT * FROM products").all(); 
+export const getAllProducts = () => db.prepare("SELECT * FROM products").all();
 
-export const saveProduct = (name, price, amount) => db.prepare("INSERT INTO product (name, price, amount) VALUES(?,?,?)").run(name, price, amount); 
+export const saveProduct = (name, price, amount) =>
+  db
+    .prepare("INSERT INTO product (name, price, amount) VALUES(?,?,?)")
+    .run(name, price, amount);
 
-export const updateProduct = (id, name, price, amount) => db.prepare("UPDATE product SET name = ?, price = ?, amount = ? WHERE id=?").run(name, price, amount, id)
+export const updateProduct = (id, name, price, amount) =>
+  db
+    .prepare("UPDATE product SET name = ?, price = ?, amount = ? WHERE id=?")
+    .run(name, price, amount, id);
